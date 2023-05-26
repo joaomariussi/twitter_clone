7346f4fbfccf1e496f4f96f45062ba91af707aa7
João Pedro Mariussi <joaomariussi10@gmail.com>
2023-05-26 09:37:45 -0300

    :rocket: Ajustes nas funções "inscreverse" e "registrar".

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -16,6 +16,47 @@ class IndexController extends  Action
     }
 
     public function inscreverse() {
+
+        $this->view->usuario = array(
+            'nome' => '',
+            'email' => '',
+            'senha' => '',
+        );
+
+        $this->view->erroCadastro = false;
+
         $this->render('inscreverse');
+    }
+
+    public function registrar() {
+
+        $usuario = Container::get_Model('Usuario');
+
+        $nome = $_POST['nome'] ?? '';
+        $email = $_POST['email'] ?? '';
+        $senha = $_POST['senha'] ?? '';
+
+        $usuario->__set('nome', $nome);
+        $usuario->__set('email', $email );
+        $usuario->__set('senha', $senha);
+
+        if ($usuario->validarCadastro() && count($usuario->recuperarUsuarioPorEmail()) == 0) {
+
+                $usuario->salvar();
+
+                $this->render('cadastro');
+
+        } else {
+
+            $this->view->usuario = array(
+                'nome' => $nome,
+                'email' => $email,
+                'senha' => $senha
+            );
+
+            $this->view->erroCadastro = true;
+
+            $this->render('inscreverse');
+        }
     }
 }
