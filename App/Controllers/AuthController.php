ced67729f04e2417f4f5257f48e03f82321fac91
João Pedro Mariussi <joaomariussi10@gmail.com>
2023-05-29 10:21:13 -0300

    :rocket: Criando uma função de autenticar o usuário no meu AuthControlle

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -8,4 +8,19 @@ use MF\Model\Container;
 
 class AuthController extends Action {
 
+    public function autenticar() {
+
+        $usuario = Container::get_Model('Usuario');
+
+        $usuario->__set('email', $_POST['email']);
+        $usuario->__set('senha', $_POST['senha']);
+
+        $retorno = $usuario->autenticar();
+        echo '<pre>';
+        print_r($retorno);
+        echo '</pre>';
+
+
+    }
+
 }
