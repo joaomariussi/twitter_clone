e83d07042d2f6d225f43252974653ce49d287fb6
João Pedro Mariussi <joaomariussi10@gmail.com>
2023-06-01 09:52:44 -0300

    :lock: Protegendo os acessos caso algum usuário acesse a timeline direto pela rota de URL.

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -16,11 +16,20 @@ class AuthController extends Action {
         $usuario->__set('senha', $_POST['senha']);
 
         $retorno = $usuario->autenticar();
-        echo '<pre>';
-        print_r($retorno);
-        echo '</pre>';
 
+        if ($usuario->__get('id') != '' && $usuario->__get('nome')) {
 
+            session_start();
+
+            $_SESSION['id'] = $usuario->__get('id');
+            $_SESSION['nome'] = $usuario->__get('nome');
+
+            header('Location: /timeline');
+
+        } else {
+            header('Location: /?login=erro
+            ');
+        }
     }
 
 }
