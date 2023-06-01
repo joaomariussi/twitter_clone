be023914e5c52e9eb42f2245ec74a16001538aa4
João Pedro Mariussi <joaomariussi10@gmail.com>
2023-06-01 10:29:56 -0300

    :rocket: Criando uma função de sair e destruindo a sessão do usuário.

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -32,4 +32,11 @@ class AuthController extends Action {
         }
     }
 
+    public function sair() {
+
+        session_start();
+        session_destroy();
+        header('Location: /');
+    }
+
 }
