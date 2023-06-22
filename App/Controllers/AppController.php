4ef41ea02c873bd2253ab765ef892619cdb49e27
João Pedro Mariussi <joaomariussi10@gmail.com>
2023-06-22 08:31:15 -0300

    :construction: Criando uma ação de seguir o usuário e deixar de seguir.

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -58,6 +58,7 @@ class AppController extends Action {
 
             $usuario = Container::getModel('Usuario');
             $usuario->__set('nome', $pesquisaPor);
+            $usuario->__set('id', $_SESSION['id']);
             $usuarios = $usuario->getAll();
         }
 
@@ -66,9 +67,23 @@ class AppController extends Action {
         $this->render('quemSeguir');
     }
 
+    public function acao() {
 
+        $this->validaAutenticacao();
 
+        $acao = isset($_GET['acao']) ? $_GET['acao'] : '';
+        $id_usuario_seguindo = isset($_GET['id_usuario']) ? $_GET['id_usuario'] : '';
 
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('id', $_SESSION['id']);
+
+        if ($acao == 'seguir') {
+            $usuario->seguirUsuario($id_usuario_seguindo);
+
+        } elseif ($acao == 'deixar_de_seguir') {
+            $usuario->deixarSeguirUsuario($id_usuario_seguindo);
+        }
+    }
 
 
 
