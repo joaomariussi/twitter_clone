b06ae44b459f0cf7e4474c15689af16cd5c74a7b
João Pedro Mariussi <joaomariussi10@gmail.com>
2023-06-28 14:12:35 -0300

    :construction: Estendendo da classe Usuario.php e setando os métodos por trazer as informações do usuário na tela.

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -2,15 +2,29 @@
 
 namespace App\Controllers;
 
+use App\Models\Tweet;
+use App\Models\Usuario;
 use MF\Controller\Action;
 use MF\Model\Container;
 class AppController extends Action {
 
+    public function validaAutenticacao() {
+
+        session_start();
+
+        if (!isset($_SESSION['id']) || $_SESSION['id'] == '' || !isset($_SESSION['nome']) || $_SESSION['nome'] == '') {
+            header('Location: /?login=erro');
+        }
+    }
+
     public function timeline() {
+
+        /**
+         * @var $tweet Tweet
+         */
 
         $this->validaAutenticacao();
 
-        //recuperação dos tweets
         $tweet = Container::getModel('Tweet');
 
         $tweet->__set('id_usuario', $_SESSION['id']);
@@ -19,6 +33,27 @@ class AppController extends Action {
 
         $this->view->tweets = $tweets;
 
+        /**
+         * @var $usuario Usuario
+         */
+
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('id', $_SESSION['id']); //Seta se o id do usuário é o mesmo da sessão.
+
+        //Seta a view e chama o getInfoUsuario da minha classe Usuario.
+        $this->view->info_usuario = $usuario->getInfoUsuario();
+
+        //Seta a view e chama o getTotalTweets da minha classe Usuario.
+        $this->view->total_tweets = $usuario->getTotalTweets();
+
+        //Seta a view e chama o getUsariosSeguindo da minha classe Usuario.
+        $this->view->total_seguindo = $usuario->getUsariosSeguindo();
+
+        //Seta a view e chama o getTotalSeguidores da minha classe Usuario.
+        $this->view->total_seguidores = $usuario->getTotalSeguidores();
+
+
+        //rendeniza a view timeline.
         $this->render('timeline');
 
     }
@@ -32,23 +67,30 @@ class AppController extends Action {
         $tweet->__set('tweet', $_POST['tweet']);
         $tweet->__set('id_usuario', $_SESSION['id']);
 
+        /**
+         * @var $tweet Tweet
+         */
+
         $tweet->salvar();
 
         header('Location: /timeline');
-    }
 
-    public function validaAutenticacao() {
+        $id_tweet = $_POST['id_tweet'];
 
-        session_start();
+        $tweet = Container::getModel('Tweet');
+        $tweet->__set('id', $id_tweet);
+        $tweet->remover();
 
-        if (!isset($_SESSION['id']) || $_SESSION['id'] == '' || !isset($_SESSION['nome']) || $_SESSION['nome'] == '') {
-            header('Location: /?login=erro');
-        }
+        header('Location: /timeline');
     }
 
     public function quemSeguir() {
 
         $this->validaAutenticacao();
+
+        /**
+         * @var $usuario Tweet
+         */
 
         $pesquisaPor = isset($_GET['pesquisaPor']) ? $_GET['pesquisaPor'] : '';
 
@@ -63,6 +105,24 @@ class AppController extends Action {
         }
 
         $this->view->usuarios = $usuarios;
+
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('id', $_SESSION['id']); //Seta se o id do usuário é o mesmo da sessão.
+
+        /**
+         * @var $usuario Usuario
+         */
+        //Seta a view e chama o getInfoUsuario da minha classe Usuario.
+        $this->view->info_usuario = $usuario->getInfoUsuario();
+
+        //Seta a view e chama o getTotalTweets da minha classe Usuario.
+        $this->view->total_tweets = $usuario->getTotalTweets();
+
+        //Seta a view e chama o getUsariosSeguindo da minha classe Usuario.
+        $this->view->total_seguindo = $usuario->getUsariosSeguindo();
+
+        //Seta a view e chama o getTotalSeguidores da minha classe Usuario.
+        $this->view->total_seguidores = $usuario->getTotalSeguidores();
 
         $this->render('quemSeguir');
     }
@@ -83,12 +143,8 @@ class AppController extends Action {
         } elseif ($acao == 'deixar_de_seguir') {
             $usuario->deixarSeguirUsuario($id_usuario_seguindo);
         }
+
+        header('Location: /quem_seguir');
     }
-
-
-
-
-
-
 
 }
