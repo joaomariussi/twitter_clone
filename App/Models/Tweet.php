6cbed8110714855d7cc079f99f311506e6226e7c
João Pedro Mariussi <joaomariussi10@gmail.com>
2023-06-28 14:20:02 -0300

    :construction: Criando uma nova função para remover o tweet.

--- App/Models/Tweet.php
+++ App/Models/Tweet.php
@@ -20,7 +20,14 @@ class Tweet extends Model {
     }
 
     //salva o tweet no banco de dados.
-    public function salvar() {
+    public function salvar(): static
+    {
+
+        $tweet = trim($this->__get('tweet')); // Remove espaços em branco do início e do final do tweet
+
+        if (empty($tweet)) { // Verifica se o tweet está vazio após remover os espaços em branco
+            return $this; // Retorna o objeto Tweet sem salvar no banco de dados
+        }
 
         $query = "insert into tweets(id_usuario, tweet) values(:id_usuario, :tweet)";
         $stmt = $this->db->prepare($query);
@@ -31,8 +38,20 @@ class Tweet extends Model {
         return $this;
     }
 
+    public function remover(): static
+    {
+        $query = "DELETE FROM tweets WHERE id = :id";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':id', $this->__get('id'));
+        $stmt->execute();
+
+        return $this;
+    }
+
+
     //recupera os registro de tweets dos usuários.
-    public function getAll() {
+    public function getAll()
+    {
 
         $query = "select
                     t.id, t.id_usuario, u.nome, t.tweet, date_format(t.data, '%d/%m/%Y %H:%i') as data
@@ -40,6 +59,7 @@ class Tweet extends Model {
                     left join usuarios as u on (t.id_usuario = u.id)
                 where 
                     t.id_usuario = :id_usuario
+                or t.id_usuario in (select id_usuario_seguindo from usuarios_seguidores where id_usuario = :id_usuario)
                 order by 
                     t.data desc";
 
