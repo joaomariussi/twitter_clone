8a0c00d092811b06a76d2e8c05c31987f4dfc7b6
joaomariussi <joaomariussi10@gmail.com>
2023-07-14 15:29:42 -0300

    :construction: Adicionando os tweets e a possibilidade de tweetar e remover o tweet na minha função minhaConta, no final, rendniza a inha view minhaConta.phtml.

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -60,6 +60,8 @@ class AppController extends Action {
 
         $this->view->localidade = $usuario->getLocalizacao();
 
+        $this->view->nome_usuario = $usuario->getNomeUsuario();
+
         //rendeniza a view timeline.
         $this->render('timeline');
 
@@ -151,19 +153,55 @@ class AppController extends Action {
             $usuario->deixarSeguirUsuario($id_usuario_seguindo);
         }
 
-        header('Location: /quem_seguir');
     }
 
     public function minhaConta() {
 
         $this->validaAutenticacao();
 
-        $usuario = Container::getModel('Usuario');
-        $usuario->__set('id', $_SESSION['id']); //Seta se o id do usuário é o mesmo da sessão.
+        /**
+         * @var $tweet Tweet
+         */
+
+        // Verifica se a requisição é do tipo POST
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $tweet = Container::getModel('Tweet');
+
+            // Verifica se o campo 'tweet' foi preenchido no formulário
+            if (isset($_POST['tweet'])) {
+                // Salva um novo tweet
+                $tweet->__set('tweet', $_POST['tweet']);
+                $tweet->__set('id_usuario', $_SESSION['id']);
+                $tweet->salvar();
+
+                //Redireciona o usuário para a mesma tela, assim não duplica o tweet quando atualizar a página.
+                header('Location: /minha_conta');
+            }
+
+            // Verifica se foi enviado o ID do tweet a ser removido
+            if (isset($_POST['id_tweet'])) {
+                // Remove um tweet existente
+                $id_tweet = $_POST['id_tweet'];
+                $tweet->__set('id', $id_tweet);
+                $tweet->remover();
+
+                header('Location: /minha_conta');
+            }
+        }
+
+        // Busca os tweets do usuário
+        $tweet = Container::getModel('Tweet');
+        $tweet->__set('id_usuario', $_SESSION['id']);
+        $tweets = $tweet->getAll();
+        $this->view->tweets = $tweets;
 
         /**
          * @var $usuario Usuario
          */
+
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('id', $_SESSION['id']); //Seta se o id do usuário é o mesmo da sessão.
+
         //Seta a view e chama o getInfoUsuario da minha classe Usuario.
         $this->view->info_usuario = $usuario->getInfoUsuario();
 
@@ -182,9 +220,13 @@ class AppController extends Action {
         //Seta a view e chama a variável $dataNascFormatada.
         $this->view->dataNascFormatada = $dataNascFormatada;
 
+        //Seta a variável e chama o getLocalizacao da minha classe Usuario.
         $this->view->localidade = $usuario->getLocalizacao();
 
+        //Seta a variável e chama o getNomeUsuario da minha classe Usuario.
+        $this->view->nome_usuario = $usuario->getNomeUsuario();
+
+        //rendeniza a view minhaConta.
         $this->render('minhaConta');
     }
-
 }
